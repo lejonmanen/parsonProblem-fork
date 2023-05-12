78512d0bdf178b857f7436deedd3759f0864ed04
David Svensson <davidsvson@gmail.com>
2023-05-12 21:29:31 +0200

    cleanup input component

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -3,41 +3,24 @@ import { useState } from "react";
 import { firestore } from "../main";
 import { useNavigate } from "react-router-dom";
 
-
-const Input = () => {
-    const [code, setCode] = useState('');
-
-    const placeHolder =
+const PLACEHOLDER_CODE = 
 `for (int i = 0; i < 10; i++) {
     print(i)
 }`
 
+const Input = () => {
+    const [code, setCode] = useState('');
     const navigate = useNavigate();
 
-    const shuffle = (rows: string[]): string[] => {
-        let currentIndex = rows.length, randomIndex;
-
-        while (currentIndex != 0) {
-            randomIndex = Math.floor(Math.random() * currentIndex);
-            currentIndex--;
-            [rows[currentIndex], rows[randomIndex]] = [
-                rows[randomIndex], rows[currentIndex]
-            ];
-        }
-
-        return rows;
-    }
-
     const handleShuffle = async () => {
-        const rows = shuffle(code.split('\n'));
+        let rows = shuffle(code.split('\n'));
 
-        try {
-            const docRef = await addDoc(collection(firestore, 'parsonItems'), {
-                rows: rows
-            });
-            navigate('/'+docRef.id);
-        } catch (error) {
-            console.error('Error adding document: ', error);
+        rows = trimLines(rows);
+        
+        const id = await writeToFirestore(rows);
+
+        if (id != null) {
+            navigate('/'+id);
         }
     }
 
@@ -48,11 +31,45 @@ const Input = () => {
                 cols={50}
                 value={code}
                 onChange={event => setCode(event.target.value)}
-                placeholder={placeHolder}
+                placeholder={PLACEHOLDER_CODE}
             /><br />
             <button onClick={handleShuffle}>Shuffle</button>
         </div>
     )
+}
+
+const writeToFirestore = async (rows : string[]) : Promise<string | null> => {
+    try {
+        const docRef = await addDoc(collection(firestore, 'parsonItems'), {
+            rows: rows
+        });
+        return docRef.id;
+    } catch (error) {
+        console.error('Error adding document: ', error);
+    }
+    return null;
+}
+
+const trimLines = (rows : string[] ): string[] => {
+    for (let i = 0; i < rows.length; i++) {
+        rows[i] = rows[i].trimStart();
+    }
+
+    return rows;
+}
+
+const shuffle = (rows: string[]): string[] => {
+    let currentIndex = rows.length, randomIndex;
+
+    while (currentIndex != 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+        [rows[currentIndex], rows[randomIndex]] = [
+            rows[randomIndex], rows[currentIndex]
+        ];
+    }
+
+    return rows;
 }
 
 export default Input;
--- src/components/Parson.tsx
+++ src/components/Parson.tsx
@@ -54,20 +54,48 @@ const Parson = () => {
         setDraggedItem(null);
     };
 
-    const listElements = list.map((item, index) => (
-        <li
-            key={item.id}
-            draggable
-            onDragStart={event => handleDragStart(event, item)}
-            onDragOver={handleDragOver}
-            onDrop={event => handleDrop(event, index)}
-            style={{
-                opacity: draggedItem && draggedItem.id === item.id ? 0.5 : 1,
-            }}
-        >
-            {item.text}
-        </li>
-    ))
+    // const listElements = list.map((item, index) => (
+    //     <li
+    //         key={item.id}
+    //         draggable
+    //         onDragStart={event => handleDragStart(event, item)}
+    //         onDragOver={handleDragOver}
+    //         onDrop={event => handleDrop(event, index)}
+    //         style={{
+    //             opacity: draggedItem && draggedItem.id === item.id ? 0.5 : 1,
+    //         }}
+    //     >
+    //         {item.text}
+    //     </li>
+    // ))
+
+    const listElements = list.map((item, index) => {
+        const lines = item.text.split('\n');
+        let indentedCode = '';
+    
+        for (let i = 0; i < lines.length; i++) {
+            if (i > 0 && lines[i - 1].trim().endsWith('{')) {
+                indentedCode += '    ';
+            }
+            indentedCode += lines[i] + '\n';
+        }
+    
+        return (
+            <li
+                key={item.id}
+                draggable
+                onDragStart={event => handleDragStart(event, item)}
+                onDragOver={handleDragOver}
+                onDrop={event => handleDrop(event, index)}
+                style={{
+                    opacity: draggedItem && draggedItem.id === item.id ? 0.5 : 1,
+                }}
+            >
+                <pre>{indentedCode}</pre>
+            </li>
+        );
+    });
+    
 
     return (
         <>
